b86a6d84f70bd3bb4dd8f9c8304afb1430c467be
George Pickering <9803299+bigpick@users.noreply.github.com>
2021-02-19 09:04:18 -0500

    scroll to top button

--- static/js/button.js
+++ static/js/button.js
@@ -1,10 +1,8 @@
-// Some code could be here ...
 //Get the button:
 mybutton = document.getElementById("scrollTopButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
